8ae01168194b7b2158f8b3f4b292f5e7ad0bebba
vishal prajapati <vishalprajapat818283@gmail.com>
2026-08-21 13:15:42 +0530

    adding package and cors problem

--- web/backend/index.js
+++ web/backend/index.js
@@ -2,8 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 const connectDB = require("./config/db");
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use(express.json());
 
